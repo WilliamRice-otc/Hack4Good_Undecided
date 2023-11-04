90ea0c07ac35f87f631d66bcea898a01881b1e44
Zachery <xacheri@outlook.com>
2023-11-03 21:21:43 -0500

    initial commit

---
+++ app/src/main/java/com/example/myfirstapp/ThirdFragment.java
@@ -0,0 +1,50 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ThirdFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ThirdFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    public ThirdFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ThirdFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ThirdFragment newInstance() {
+        ThirdFragment fragment = new ThirdFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+}
