e05b4d2006fd1fb79d05ee6d29b68bca1fb76db1
sm0862512 <sm0862512@otc.edu>
2023-11-04 04:45:00 -0500

    working on user fourm

---
+++ website/moduals/userlookup.php
@@ -0,0 +1,9 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="tests.css">
+
+<div class="title1">
+<h1 type="text/css"  class=" alert alert-primary rounded">Userlookup</h1>
+<input class="user-lookup-1">
+
+   <div class="selectpatting">
+       <p class="btn btn-primary ">search</p></div></div>
